cdc87e266a2bfeec696afb221f5ad980cf096ae3
InkLemons <119631995+InkLemons@users.noreply.github.com>
2024-10-28 13:31:20 -0600

    10.28.24
    Added a small description and worked on footer styles.

--- JS/footer.js
+++ JS/footer.js
@@ -3,16 +3,15 @@ const createFooter = () => {
 
     footer.innerHTML = `
         <footer>
-            <img id="footer-img" src="IMGS/TypeHavenSwirl.png" alt="Footer Image">
             <section class="foot-links">
                 <table class="overlay-table">
                     <tr>
-                        <td><a href="aboutus.html" class="foot-link">About Us</a></td>
-                        <td><a href="fieldtrips.html" class="foot-link">Field Trips</a></td>
+                        <td><strong><a href="aboutus.html" class="foot-link">About Us</a></strong></td>
+                        <td><strong><a href="profile.html" class="foot-link">profile</a></strong></td>
                     </tr>
                     <tr>
-                        <td><a href="groups.html" class="foot-link">Teacher Login</a></td>
-                        <td><a href="contact.html" class="foot-link">Student Login</a></td>
+                        <td><strong><a href="teach-login.html" class="foot-link">Teacher Login</a></strong></td>
+                        <td><strong><a href="stu-login.html" class="foot-link">Student Login</a></strong></td>
                     </tr>
                 </table>
             </section>
@@ -23,7 +22,7 @@ const createFooter = () => {
                     <img src="#" class="SM-Icon" alt="Facebook">
                 </div>
             </section>
-            <p class="copyright">©2024 InkLemons</p>
+            <p class="copyright">&#169;2024 InkLemons</p>
         </footer>
     `;
 }
