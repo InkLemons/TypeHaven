40443d2ec0ab983903188bf2ce5abfb4a049fc4d
InkLemons <119631995+InkLemons@users.noreply.github.com>
2024-11-04 13:35:03 -0700

    11.4.24
    Finished style for login screen
    -Show password
    -Login with google button
    -text for incorrect

--- JS/nav.js
+++ JS/nav.js
@@ -5,14 +5,16 @@ const createNav = () => {
         <div class="nav">
             <a href="index.html" id="TH-LOGO-HOLDER"><img src="IMGS/TypeHavenLogo.svg" class="TH-LOGO" alt="TypeHaven"></a>
             <div class="nav-items">
-                <a href="index.html"><img src="#" class="home-btn" alt="#"></a>
+                <a href="index.html"><img src="IMGS/PROFILE_IMG.svg" class="profile-btn" alt="#"></a>
             </div>   
         </div>
         <ul class="link-container">
-            <li class="link-item"><a href="" class="link">Login</a></li>
-            <li class="link-item"><a href="" class="link">Classes</a></li>
             <li class="link-item"><a href="" class="link">Profile</a></li>
+            <li class="link-item"><a href="" class="link">Classes</a></li>
             <li class="link-item"><a href="" class="link">Help</a></li>
+            <li class="link-item"><a href="stu-login.html" class="link">Student Login</a></li>
+            <li class="link-item"><a href="teach-login.html" class="link">Teacher Login</a></li>
+
         <ul>  
     `;
 }
