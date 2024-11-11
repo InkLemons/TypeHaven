a3d7fff891e1af543a8bc9f4a48a0d0c480a9dbd
InkLemons <119631995+InkLemons@users.noreply.github.com>
2024-11-11 13:40:07 -0700

    11.11.24
    Moved styles to own stylesheets for legibility purposes.
    Worked a little bit more on profile page

--- JS/nav.js
+++ JS/nav.js
@@ -5,11 +5,11 @@ const createNav = () => {
         <div class="nav">
             <a href="index.html" id="TH-LOGO-HOLDER"><img src="IMGS/TypeHavenLogo.svg" class="TH-LOGO" alt="TypeHaven"></a>
             <div class="nav-items">
-                <a href="index.html"><img src="IMGS/PROFILE_IMG.svg" class="profile-btn" alt="#"></a>
+                <a href="profile.html"><img src="IMGS/PROFILE_IMG.svg" class="profile-btn" alt="#"></a>
             </div>   
         </div>
         <ul class="link-container">
-            <li class="link-item"><a href="" class="link">Profile</a></li>
+            <li class="link-item"><a href="profile.html" class="link">Profile</a></li>
             <li class="link-item"><a href="class-list.html" class="link">Classes</a></li>
             <li class="link-item"><a href="" class="link">Help</a></li>
             <li class="link-item"><a href="stu-login.html" class="link">Student Login</a></li>
